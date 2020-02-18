a8689b5bb6a928ad23be9efe4c27c1c71021b033
Pavel <pahan.rolch@mail.ru>
2020-02-18 23:01:12 +0300

    add angular material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { PostsCreateComponent } from './posts/posts-create/posts-create.componen
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
